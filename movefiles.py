999f03ff26b291ed3a5fa1573bea9d09966c3ced
RisheeDissaPro <50660696+RisheeDissaPro@users.noreply.github.com>
2023-01-11 00:41:40 +0530

    Update movefiles.py

--- movefiles.py
+++ movefiles.py
@@ -1,11 +1,18 @@
 import os
 import shutil
 
-#Folder path defined
-os.chdir('/Users/RISHEE/Desktop/Test')
+# Folder path defined for os module to identify 
+folder_path = r"C:\Users\RISHEE\Desktop\Test"
 
-for item in os.listdir():
-     file_name = item[0:4]
-     if not os.path.exists(file_name):
-          os.mkdir(file_name)
-     shutil.move(os.path.join(item), os.path.join(file_name))
+if (os.path.exists(folder_path)):
+     os.chdir(folder_path)
+     # for loop will iterate through all the files in the defined path
+     for item in os.listdir():
+          # Seperate first 4 strings of the folder name
+          road_name = item[0:4]
+          if not os.path.exists(road_name):
+               os.mkdir(road_name)
+          shutil.move(item, road_name)
+          print(f"{road_name} Successfully moved")
+else:
+     print("Folder Path is incorrect or does not exist")
