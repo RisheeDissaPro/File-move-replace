e004ea930ee6805b820af4502538f36d1924bf7c
RisheeDissaPro <50660696+RisheeDissaPro@users.noreply.github.com>
2023-01-11 00:55:24 +0530

    Update rename.py

--- rename.py
+++ rename.py
@@ -1,19 +1,20 @@
 import os
 import openpyxl
 
-os.chdir('/Users/RISHEE/Desktop/Test')
-# os.chdir('C:\Users\RISHEE\Desktop\Test')
+# Folder path defined for os module to identify 
+folder_path = r"C:\Users\RISHEE\Desktop\Test"
 name_path = "D:\Road Names.xlsx"
 
 wb = openpyxl.load_workbook(name_path)
 worksheet = wb['Sheet1']
 
+os.chdir('/Users/RISHEE/Desktop/Test')
 for item in os.listdir():
     for cell in worksheet['A']:
         if cell.value == item:
             row_cordinate = cell.row
             column_cordinate = cell.column+1
-            # print(row_cordinate, column_cordinate)
-            print(worksheet.cell(row_cordinate, column_cordinate).value)
-
-    
+            road_name = worksheet.cell(row_cordinate, column_cordinate).value
+            new_name = f"{item} - {road_name}"
+            os.rename(item, new_name ) 
+            print(f"{item} Successfully renamed")
